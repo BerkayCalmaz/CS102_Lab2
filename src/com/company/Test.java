208ea14eb1f708433270608899d286db5aa0963e
Berkay Calmaz <berkay.calmaz@ug.bilkent.edu.tr>
2020-10-22 22:57:08 +0300

    Test part is updated.

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -12,19 +12,23 @@ public class Test {
     HTMLFilteredReader myReader3 = new HTMLFilteredReader("https://scrapethissite.com/pages/simple/");
     wordSearchClass myReader4 = new wordSearchClass("https://scrapethissite.com/pages/simple/");
         //-----PART B-----
-//        System.out.println( "Name of the website is: " + myReader1.getName() );
-//        System.out.println( "The url is: " + myReader1.getURL() );
-//        System.out.println( myReader2.getPageContents() );
+        System.out.println("-------------------- PART B --------------------");
+        System.out.println( "Name of the website is: " + myReader1.getName() );
+        System.out.println( "The url is: " + myReader1.getURL() );
+        System.out.println( myReader2.getPageContents() );
 
         //-----PART C-----
-//        System.out.println( myReader2.getNumberOfCSSLinks() );
+        System.out.println("-------------------- PART C --------------------");
+        System.out.println( myReader2.getNumberOfCSSLinks() );
 
         //-----PART D-----
-        // System.out.println(myReader3.getUnfilteredPageContents());
+        System.out.println("-------------------- PART D --------------------");
+//        System.out.println(myReader3.getUnfilteredPageContents());
 
         //-----PART E------
-        System.out.println( myReader4.findWord("Ankara" ) );
-        System.out.println( myReader4.findWord("Turkey" ) );
+        System.out.println("-------------------- PART E --------------------");
+        System.out.println( "The index of Ankara is:" + myReader4.findWord("Ankara" ) );
+        System.out.println( "The index of Turkey is:" + myReader4.findWord("Turkey" ) );
 
 
     }
